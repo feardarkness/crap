f2d15b16797817bbd170c6970935639544da3bc4
Ariel Alvarado <aalvarado@agetic.gob.bo>
2016-08-26 19:36:47 -0400

    aaaaa

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var request = require('request');
 var _ = require('underscore');
 
@@ -5,21 +6,29 @@ var cheerio = require('cheerio')
 
 var rand = Math.random();
 var fundempresaUrl = "http://portal2.fundempresa.org.bo/portal/sitio/consultasWeb.jsp?tipoConsulta=SLEmpresas&random=" + rand;
-
+/*
+console.log(fundempresaUrl);
+return;
+*/
 var j = request.jar();
 var options = {
 	url: fundempresaUrl,
 	method: 'GET',
 	headers: {
-		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0',
-		'Connection': 'keep-alive'
+		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.108 Safari/537.36",
+		//'Connection': 'keep-alive'
 	},
-	followRedirect: false,
+	followRedirect: true,
+	followAllRedirects: true,
 	forever: true,
 	//jar: j,
+	jar: true,
 };
 
+
+
 request(options, function (error, response, body) {
+	console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 /*
 	var cookies = j.getCookies(fundempresaUrl);
 	console.log("++++++++++++++++++++++++++++++++++++++++++++++++");
@@ -32,7 +41,6 @@ request(options, function (error, response, body) {
 */
 	console.log("+++++++++++++++++++++++++++++++");
 	console.log(response);
-	console.log(response.status);
 	console.log('+++++++++++++++++++++++++++++++++++++++++')
 
 	if (error){
@@ -46,30 +54,64 @@ request(options, function (error, response, body) {
 		//console.log(response.headers['set-cookie'][0].split(";")[0]);
 		//return;
 
+		console.log("+++++++++++++++++++++++++++++++");
+		console.log(response.req);
+		console.log("+++++++++++++++++++++++++++++++");
+		console.log(response.headers['set-cookie']);
+		console.log("+++++++++++++++++++++++++++++++");
+		console.log(thaCokeeee);
+		console.log("+++++++++++++++++++++++++++++++");
+		console.log( response.req._headers );
+		console.log("+++++++++++++++++++++++++++++++");
+
 		var $ = cheerio.load(body);
 		var dtLoginHidden = $('#dtLoginHidden').val();
-		var nuSecHidden = $('#nuSecHidden').val();
+		var nuSecHidden = $('#nuSecHidden').val().replace(" ", "");	// no se necesita el replace
 
 		var options = {
-			url: "http://portal2.fundempresa.org.bo/portal/servlet/SLPago",
+			//url: "http://portal2.fundempresa.org.bo/portal/servlet/SLPago",
 			//url: "http://portal2.fundempresa.org.bo/portal/servlet/SLPago?nombre=hector&nombre-ISN-DDE=0&matricula=&idMatricula=empresas&servlet=SLBasica&matricula-ISN-DDE=0&nit=&NIT-DDE=&licFunc=&licFunc-ISN-DDE=0&submit1=PorNombre&nuSecHidden=2016082416044255509683167688402251",
-			method: 'POST',
+			//url: "http://portal2.fundempresa.org.bo/portal/servlet/SLEmpresasList",
+			url : "http://portal2.fundempresa.org.bo/portal/servlet/SLPago?nombre=hotel&nombre-ISN-DDE=0&matricula=&idMatricula=empresas&servlet=SLBasica&matricula-ISN-DDE=0&nit=&NIT-DDE=&licFunc=&licFunc-ISN-DDE=0&submit1=PorNombre&nuSecHidden=" + nuSecHidden,
+			method: 'GET',
 			headers: {
-				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0',
-				'Connection': 'keep-alive',
+				//'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0',
+				//'Connection': 'keep-alive',
 				//'Cookie' : 'JSESSIONID=3E90DBFB6BB6E9D6ACBECCC3AE6554BF',
-				'Cookie' : thaCokeeee,
+				//'Cookie': thaCokeeee,
+				//'content-type': 'application/x-www-form-urlencoded',
+				"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+				"Accept-Encoding": "gzip, deflate, sdch",
+				"Accept-Language": "en-US,en;q=0.8",
+				"Cache-Control": "no-cache",
+				"Connection": "keep-alive",
+				//"Cookie": "JSESSIONID=56C8D1BB5EA3BDB402A11D21246B1E86; __utma=228589322.1435119121.1466609502.1472042514.1472163439.11; __utmc=228589322; __utmz=228589322.1471274563.8.8.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)",
+				"Host": "portal2.fundempresa.org.bo",
+				"Pragma": "no-cache",
+				"Referer": "http://portal2.fundempresa.org.bo/portal/servlet/SLEmpresasList",
+				"Upgrade-Insecure-Requests": "1",
+				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.108 Safari/537.36",
 			},
-			followRedirect: false,
+			followRedirect: true,
 			forever: true,
 			//jar: j,
-			formData: {
-				"cdUsuario": "usuarioweb",
-				"cdClave": "cons123",
-				"tipoConsulta": "SLEmpresas",
-				"nuSecHidden": dtLoginHidden,
-				"dtLoginHidden": nuSecHidden,
+			jar: true,
+			/*
+			form: {
+				"nombre": "hotel",
+				"nombre-ISN-DDE": "0",
+				"matricula": "",
+				"matricula-ISN-DDE": "0",
+				"nit": "",
+				"NIT-DDE": "",
+				"licFunc": "",
+				"licFunc-ISN-DDE": "0",
+				"consultaSel": "PorNombre",
+				"nuSecHidden": nuSecHidden,
+				//"nuSecHidden": dtLoginHidden,
+				//"dtLoginHidden": nuSecHidden,
 			}
+			*/
 		};
 
 		//POSTDATA=cdUsuario=usuarioweb&cdClave=cons123&tipoConsulta=SLEmpresas&nuSecHidden=2016082416512441808090247609077488&dtLoginHidden=2016-08-24+16%3A51%3A24.418
@@ -77,7 +119,6 @@ request(options, function (error, response, body) {
 		//console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
 		//console.log(response.headers['set-cookie']);
 		//console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-
 
 
 		console.log("+++++++++++++++++++++++++++++++");
@@ -89,12 +130,82 @@ request(options, function (error, response, body) {
 				console.log(error);
 				return;
 			}
+			var options = {
+				//url: "http://portal2.fundempresa.org.bo/portal/servlet/SLPago",
+				//url: "http://portal2.fundempresa.org.bo/portal/servlet/SLPago?nombre=hector&nombre-ISN-DDE=0&matricula=&idMatricula=empresas&servlet=SLBasica&matricula-ISN-DDE=0&nit=&NIT-DDE=&licFunc=&licFunc-ISN-DDE=0&submit1=PorNombre&nuSecHidden=2016082416044255509683167688402251",
+				//url: "http://portal2.fundempresa.org.bo/portal/servlet/SLEmpresasList",
+				url : "http://portal2.fundempresa.org.bo/portal/servlet/SLPago?nombre=hotel&nombre-ISN-DDE=0&matricula=&idMatricula=empresas&servlet=SLBasica&matricula-ISN-DDE=0&nit=&NIT-DDE=&licFunc=&licFunc-ISN-DDE=0&submit1=PorNombre&nuSecHidden=" + nuSecHidden,
+				method: 'GET',
+				headers: {
+					//'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0',
+					//'Connection': 'keep-alive',
+					//'Cookie' : 'JSESSIONID=3E90DBFB6BB6E9D6ACBECCC3AE6554BF',
+					//'Cookie': thaCokeeee,
+					//'content-type': 'application/x-www-form-urlencoded',
+					"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+					"Accept-Encoding": "gzip, deflate, sdch",
+					"Accept-Language": "en-US,en;q=0.8",
+					"Cache-Control": "no-cache",
+					"Connection": "keep-alive",
+					//"Cookie": "JSESSIONID=56C8D1BB5EA3BDB402A11D21246B1E86; __utma=228589322.1435119121.1466609502.1472042514.1472163439.11; __utmc=228589322; __utmz=228589322.1471274563.8.8.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)",
+					"Host": "portal2.fundempresa.org.bo",
+					"Pragma": "no-cache",
+					"Referer": "http://portal2.fundempresa.org.bo/portal/servlet/SLEmpresasList",
+					"Upgrade-Insecure-Requests": "1",
+					"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.108 Safari/537.36",
+				},
+				followRedirect: true,
+				forever: true,
+				//jar: j,
+				jar: true,
+				/*
+				form: {
+					"nombre": "hotel",
+					"nombre-ISN-DDE": "0",
+					"matricula": "",
+					"matricula-ISN-DDE": "0",
+					"nit": "",
+					"NIT-DDE": "",
+					"licFunc": "",
+					"licFunc-ISN-DDE": "0",
+					"consultaSel": "PorNombre",
+					"nuSecHidden": nuSecHidden,
+					//"nuSecHidden": dtLoginHidden,
+					//"dtLoginHidden": nuSecHidden,
+				}
+				*/
+			};
+
+			//POSTDATA=cdUsuario=usuarioweb&cdClave=cons123&tipoConsulta=SLEmpresas&nuSecHidden=2016082416512441808090247609077488&dtLoginHidden=2016-08-24+16%3A51%3A24.418
+
+			//console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+			//console.log(response.headers['set-cookie']);
+			//console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+
 			console.log("+++++++++++++++++++++++++++++++");
-			console.log(response.req);
-			//console.log(response.req);
 			//console.log(body);
-			//console.log(response.headers);
+			console.log(response.headers);
+
+			request(options, function(error, response, body){
+				if (error){
+					console.log(error);
+					return;
+				}
+				console.log("+++++++++++++++++++++++++++++++");
+				console.log( response.req._headers );
+				console.log("+++++++++++++++++++++++++++++++");
+				console.log( _.keys(response) );
+				console.log("+++++++++++++++++++++++++++++++");
+				console.log( response );
+				//console.log(response.headers);
+			});
 		});
+
+		
+
+
+		
 
 	}
 
---
+++ index2.js
@@ -0,0 +1,124 @@
+var request = require('request');
+var _ = require('underscore');
+
+var cheerio = require('cheerio')
+
+var rand = Math.random();
+var fundempresaUrl = "http://portal2.fundempresa.org.bo/portal/sitio/consultasWeb.jsp?tipoConsulta=SLEmpresas&random=" + rand;
+
+var j = request.jar();
+
+var options = {
+	url: fundempresaUrl,
+	method: 'GET',
+	headers: {
+		"Host": "portal2.fundempresa.org.bo",
+		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0",
+		"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+		"Accept-Language": "en-US,en;q=0.5",
+		"Accept-Encoding": "gzip, deflate",
+		"Referer": "http://fundempresa.org.bo/",
+		//"Connection": "keep-alive",
+		"Upgrade-Insecure-Requests": "1",
+	},
+	followRedirect: true,
+	forever: true,
+	jar: j,
+};
+
+request(options, function (error, response, body) {
+	var $ = cheerio.load(body);
+	var nuSecHidden = $("#nuSecHidden").val();
+	var dtLoginHidden = $("#dtLoginHidden").val();
+
+	var options = {
+		url: "http://portal2.fundempresa.org.bo/portal/servlet/SLFramePrincipalWeb",
+		method: 'POST',
+		headers: {
+			"Host": "portal2.fundempresa.org.bo",
+			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0",
+			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+			"Accept-Language": "en-US,en;q=0.5",
+			"Accept-Encoding": "gzip, deflate",
+			"Referer": "http://fundempresa.org.bo/",
+			"Upgrade-Insecure-Requests": "1",
+		},
+		followRedirect: true,
+		forever: true,
+		jar: j,
+		form: {
+			"cdUsuario": "usuarioweb",
+			"cdClave": "cons123",
+			"tipoConsulta": "SLEmpresas",
+			"nuSecHidden": nuSecHidden,
+			"dtLoginHidden": dtLoginHidden,
+		}
+	};
+
+	request(options, function (error, response, body) {
+		var $ = cheerio.load(body);
+		var frameUrl = $("#WORK").prop('src');		// http://portal2.fundempresa.org.bo:80/portal/servlet/SLEmpresas?nuSecHidden=20160826172503098008409301377929779
+		var options = {
+			url: frameUrl,
+			method: 'GET',
+			headers: {
+				"Host": "portal2.fundempresa.org.bo",
+				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0",
+				"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+				"Accept-Language": "en-US,en;q=0.5",
+				"Accept-Encoding": "gzip, deflate",
+				"Referer": "http://portal2.fundempresa.org.bo/portal/servlet/SLFramePrincipalWeb",
+				"Upgrade-Insecure-Requests": "1",
+			},
+			followRedirect: true,
+			forever: true,
+			jar: j,
+		};
+
+		request(options, function (error, response, body) {
+			var nuSecHidden2 = frameUrl.split('=')[1];
+			var options = {
+				url: "http://portal2.fundempresa.org.bo/portal/servlet/SLEmpresasList",
+				method: 'POST',
+				headers: {
+					"Host": "portal2.fundempresa.org.bo",
+					"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0",
+					"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+					"Accept-Language": "en-US,en;q=0.5",
+					"Accept-Encoding": "gzip, deflate",
+					"Referer": frameUrl,
+					"Upgrade-Insecure-Requests": "1",
+				},
+				followRedirect: true,
+				forever: true,
+				jar: j,
+				form: {
+					"nombre": "casa",
+					"nombre-ISN-DDE": "0",
+					"matricula": "",
+					"matricula-ISN-DDE": "0",
+					"nit": "",
+					"NIT-DDE": "",
+					"licFunc": "",
+					"licFunc-ISN-DDE": "0",
+					"consultaSel": "PorNombre",
+					"nuSecHidden": nuSecHidden2,
+				}
+			};
+
+			request(options, function (error, response, body) {
+				console.log(response.req._headers);
+				console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++");
+				console.log(response.headers);
+				console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++");
+				console.log(body);
+				console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++");
+				console.log(nuSecHidden);
+			});
+
+		});
+
+	});
+
+});
+
